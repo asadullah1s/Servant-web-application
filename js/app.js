8ee73c27f300e993b1526af698e29b035a877869
Muhammad Asadullah Qureshi <113957752+asadullah1s@users.noreply.github.com>
2026-09-08 02:01:07 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-
+async function loadPartials() {
+    // Load Navbar
+    const navbar = document.getElementById('navbar-container');
+    if (navbar) {
+        const res = await fetch('partials/navbar.html');
+        navbar.innerHTML = await res.text();
+    }
+    // Load Footer
+    const footer = document.getElementById('footer-container');
+    if (footer) {
+        const res = await fetch('partials/footer.html');
+        footer.innerHTML = await res.text();
+    }
+    // ... other partials
+    // Re-run language apply because new elements with data-i18n are added
+    applyLanguage(currentLang);
+    // Re-bind language switch events because dropdown is recreated
+    initLanguageSwitch();
+}
